32729347276fa8ae156b5510005eea6393a445c2
Sok Vireak <sokvireak18@kit.edu.kh>
2024-05-11 11:52:28 +0700

    fixed sessionStorage

--- app/components/buttons/ThemeToggler.js
+++ app/components/buttons/ThemeToggler.js
@@ -1,29 +1,14 @@
 'use client'
 import { Icon } from '@iconify/react'
 import React, { useEffect, useRef, useState } from 'react'
-import { usePathname } from 'next/navigation';
-
-//custom sessionStorage hook
-export const UseSessionStorage = (key, initialValue) => {
-    const [value, setValue] = useState(() => {
-        const storedValue = sessionStorage.getItem(key);
-        return storedValue ? JSON.parse(storedValue) : initialValue;
-    });
-
-    useEffect(() => {
-        sessionStorage.setItem(key, JSON.stringify(value));
-    }, [key, value]);
-
-    return [value, setValue];
-};
 
 const ThemeToggler = () => {
   const themeOptions = useRef(null)
-  const [sessionStorageValue, setSessionStorageValue] = UseSessionStorage('theme', '');
-  const [theme, setTheme] = useState(sessionStorageValue)
+  const [theme, setTheme] = useState('')
   const [isMenuOpen, setIsMenuOpen] = useState(false)
 
   useEffect(()=>{
+    setTheme(sessionStorage.getItem("theme"))
     if(window!=undefined){
       if(theme==""){
         if(window.matchMedia('(prefers-color-scheme: dark)').matches){
@@ -58,7 +43,7 @@ const ThemeToggler = () => {
   },[isMenuOpen, theme, themeOptions])
 
   const handleChange = (mode) => {
-    setSessionStorageValue(mode=='dark'?'dark':mode=='light'?'light':'')
+    sessionStorage.setItem("theme",mode=='dark'?'dark':mode=='light'?'light':'')
     setTheme(mode=='dark'?'dark':mode=='light'?'light':'')
     themeOptions.current.classList.toggle("hidden")
     setIsMenuOpen(!isMenuOpen)
